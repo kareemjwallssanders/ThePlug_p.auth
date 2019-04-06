8f46f65338df6297d5362e869fb7eefb035e6bb3
KareemjWall-Sanders <kareemjwallssanders@gmail.com>
2019-04-06 19:41:01 -0400

    first push

---
+++ The_Plug_2/config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+
+
+module.exports = {'url' :'mongodb://demo:demo@theplug-ucnuw.mongodb.net/test?retryWrites=test', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+dbName: 'theplug'
+};
+
+// module.exports = {'url' :'mongodb+srv://demo:demo@cluster0-2mgxu.mongodb.net/test?retryWrites=true',
+// 'dbName':'sneakerreview'
+// };
+
+
+
